5056e4a131497f39d91080059bf37311e1579566
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-07-15 11:58:34 -0500

    The toString() Method

---
+++ app.js
@@ -0,0 +1,8 @@
+/****************************************************
+*                The toString() Method              *
+****************************************************/
+var x = 123;
+document.getElementById("to-string").innerHTML =
+    x.toString() + "<br>" +
+   (123).toString() + "<br>" +
+   (100 + 23).toString();
